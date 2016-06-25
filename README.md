3e22b6c0b7e67077cdade6f507285388cc19e43f
Ulrik H. Gade <uhg@ulrikgade.dk>
2016-06-25 17:57:31 +0200

    Edit .gitignore to allow pushing from root rather than src

---
+++ README.md
@@ -0,0 +1,36 @@
+Welcome to the Meetup Event Planner app by Ulrik H. Gade.
+
+USE
+
+To use the app, install the 'build' folder on your Web server and go to 'index.html' in a modern Web browser (as of early 2016). Install e.g. WAMP to run the app locally on your own machine.
+
+Choose "Sign Up" from the front page to create an account, and take it from there.
+
+If your server does not provide a secure connection, you may need to enter your default location (in Account Settings) to get location suggestions to work.
+
+Fair warning: Don't use the app for anything serious: this is a study project, not a commercial-grade service. You may very well loose all the data you enter, and have your password exposed. I am not actively maintaining the app; it may fail as time progresses.
+
+BUILD
+
+You can build the app yourself from the source using gulp in Node.js (see gulpfile.js for details).
+
+Start by running 'npm install' in Node.js from the root directory (i.e. the one where this readme file resides) to install the required modules. Ignore any NPM warnings re. deprecated versions of some dependencies: usually, this is just the module devs falling behind on maintenance, with no relevant adverse effects for this project.
+
+Then just call 'gulp' to run the default build script, which should get everything ready for you. Or, run 'gulp setup' to just get ready for development without doing a complete build.
+
+TEST
+
+After completing the build, open 'src/test/test-runner.html' in a Web browser to run the unit and UI test suites. Expect the UI part to open and close a bunch of windows and popups, and the whole thing to last app. 60-70 secs secs.
+
+To run the tests on the source files, change the setup of 'app.testutil.testTarget' in test-runner.html (see source for details). Even if only working on the source, you must run a full build at least once before the tests can run.
+
+Known limitations: 1) The autocomplete/suggest tests may create false negatives. For now, just run the suite repeatedly until they pass. 2) The automated tests mess up local storage. Use a separate browser for actual use/manual testing.
+
+
+ABOUT
+
+I created this app in early 2016 as an exercise in user-friendly front-end form design for my Senior Web Developer Nanodegree course at Udacity.com.
+
+I also used the app to experiment with cross-browser/platform, "classical" object-oriented programming in JavaScript, as well as modern build and testing tools. This ended up getting as involved as it was educational.
+
+Please see the source code, docs and UML models in my Github repo for more technical details. 
---
+++ gulpfile.js
@@ -0,0 +1,290 @@
+/* Imports */
+
+	var gulp = require('gulp');
+
+	var autoprefixer = require('gulp-autoprefixer');
+
+	//var babel = require('gulp-babel'); // ES2015 transcoder
+
+	//var browserSync = require('browser-sync').create(); // install fails on too deep dir structure
+
+	var concat = require('gulp-concat');
+
+	var cssnano = require('gulp-cssnano');
+
+	//var eslint= require('gulp-eslint'); //recommended by Udacity, using jshint for now
+
+	var frep = require('gulp-frep');
+
+	//var ftp = require( 'vinyl-ftp' );
+
+	var gulpif = require('gulp-if'); // provides conditional processing of streams
+
+	var htmlmin = require('gulp-htmlmin'); // minimizes html
+
+	var imagemin = require('gulp-imagemin'); //lossless image compression
+
+	//var pngquant = require(imagemin-pngquant); //'smart lossy image compression'
+
+	//var jasmine = require('gulp-jasmine-phantom'); // headless test framework
+
+	var jshint = require('gulp-jshint'); //also requires npm install of jshint-stylish
+
+	var less = require('gulp-less'); // LESS precompiler
+
+	var modernizr = require('gulp-modernizr'); // browser feature detection
+
+	var sass = require('gulp-sass'); // SASS precompiler
+
+	var sequence = require('gulp-sequence'); // run tasks in controlled sequence, not randomly parallel
+
+	var shell = require('gulp-shell');
+
+	//var sourcemaps = require('gulp-sourcemaps');
+
+	var uglify = require('gulp-uglify'); //minimizes js
+
+	var useref = require('gulp-useref'); // parses js and css build blocks in html source 
+
+
+/* Scripts */
+
+	gulp.task('init-build', shell.task([ // // clean out build directory
+			
+		'if exist build rmdir build /S /Q'
+	]));
+
+
+	gulp.task('build-images', function() { // copy images to build
+		
+		gulp.src('src/assets/img/*.*')
+
+		.pipe(imagemin())
+
+		.pipe(gulp.dest('build/assets/img/.'));
+	});
+
+
+	gulp.task('build-fonts', function() { // copy fonts to build
+		
+		gulp.src('node_modules/materialize-css/font/**/*.*') // materialize
+
+		.pipe(gulp.dest('build/assets/font/.'));
+
+		gulp.src('node_modules/bootstrap/fonts/*.*') // bootstrap timepicker
+
+		.pipe(gulp.dest('build/assets/fonts/.'));
+	});
+
+
+	gulp.task('build-pages', function() { // copy html to build, concatenating and minifying js and css files
+		
+		gulp.src('src/index.html') // TO-DO: exclude test runner file
+			
+			.pipe(useref()) // concatenate build blocks
+
+			.pipe(gulpif('*.js', frep( // Remove 'use strict' commands
+			[
+				{
+					pattern: /'use strict';*/g,
+					replacement: ''
+				}
+			])))
+
+			.pipe(gulpif('*.css', cssnano())) // minify css
+			
+			//.pipe(gulpif('*.html', htmlmin())) // minify html
+
+			.pipe(gulpif('*.js', uglify())) // minify js
+
+			.pipe(gulp.dest('build/.'))
+	});
+
+
+	gulp.task('fonts', function() { // copy fonts to where css expects to find them during dev
+		
+		gulp.src('node_modules/materialize-css/font/**/*.*') // materialize
+
+		.pipe(gulp.dest('src/assets/font/.'));
+
+		gulp.src('node_modules/bootstrap/fonts/*.*') // bootstrap timepicker
+
+		.pipe(gulp.dest('src/assets/fonts/.'));
+	});
+
+
+	gulp.task('less', function() { // (re-)compile less to css
+		
+		gulp.src('src/assets/less/bootstrap.less')
+			
+		.pipe(less())
+		
+		//.pipe(cssnano()) // minimize
+		
+		.pipe(gulp.dest('src/assets/css/'));
+	});
+
+	
+	gulp.task('modernizr', function() { // (re-)build custom Modernizr library
+
+		gulp.src('src/assets/**/*.js')
+
+		.pipe(modernizr('modernizr-custom.js',
+		{
+		    "cache" : true,
+		    "devFile" : false,
+		    "dest" : 'src/lib/modernizr-custom.js',
+		    "options" : [
+		        //"setClasses",
+		       // "addTest",
+		       // "html5printshiv",
+		       // "testProp",
+		       // "fnBind"
+		    ],
+		    "uglify" : false,
+		    "tests" : ['datalistelem', 'formvalidation', 'input', 'inputtypes', 'localstorage', 'matchmedia'],
+		    "excludeTests": [],
+		    "crawl" : false,
+		    "useBuffers": false,
+		    "files" : {
+		        "src": [
+		            "*[^(g|G)runt(file)?].{js,css,scss}",
+		            "**[^node_modules]/**/*.{js,css,scss}",
+		            "!lib/**/*"
+		        ]
+		    },
+		    "customTests" : []
+		}));
+	});
+
+
+	gulp.task('sass', function() { // (re-)compile sass to css
+		
+		gulp.src('src/assets/sass/*.scss')
+			
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		
+		//.pipe(autoprefixer({browsers: ['last 2 versions']})) // chokes on '//' comments, so run sass first
+				
+		.pipe(cssnano()) // minimize (again)
+		
+		.pipe(gulp.dest('src/assets/css/.'));
+	});
+
+ 
+	gulp.task('setup', sequence(['fonts', 'less', 'modernizr', 'sass'])); // get set up for development after first running npm install
+
+	
+	gulp.task('upload-dev', function() { // upload files to development Web server
+		
+		gulp.src('build/**/*.*') // build
+
+		.pipe(gulp.dest('\\\\192.168.1.108\\Music\\www\\event-planner\\build\\.'));
+
+		gulp.src('src/**/*.*') // src
+
+		.pipe(gulp.dest('\\\\192.168.1.108\\Music\\www\\event-planner\\src\\.'));
+
+		gulp.src('test/**/*.*') // test
+
+		.pipe(gulp.dest('\\\\192.168.1.108\\Music\\www\\event-planner\\test\\.'));
+	});
+	
+
+	gulp.task('validate-scripts', function() { // validate scripts
+		
+		// validate own js assets
+
+		gulp.src('src/assets/js/**/*.js')
+
+		.pipe(jshint()) // check for coding errors
+		
+		.pipe(jshint.reporter('jshint-stylish')) // make errors pretty
+
+		.pipe(jshint.reporter('fail'));
+	});
+
+	 
+	gulp.task('validate-tests', function() { 
+		
+		
+		gulp.src('src/test/*_test.js')
+		
+			// check unit tests for coding errors
+		
+			.pipe(frep([ // Remove 'use strict' commands
+				{
+					pattern: /'use strict';*/g,
+					replacement: ''
+				}
+			]))
+			
+			.pipe(jshint())
+			
+			.pipe(jshint.reporter('jshint-stylish'))
+
+			.pipe(jshint.reporter('fail'))
+
+			// run unit tests
+			
+			//.pipe(jasmine()) //doesn't seem to work - does not discover deliberate syntax error
+			//.pipe(jasmine({integration: true,vendor: 'js/**/*.js'}))
+			
+			;
+	});
+
+
+	gulp.task('docs', shell.task([ // create fresh jsDoc
+		
+		'if exist docs rmdir docs /S /Q',
+		
+		'jsdoc src/assets/js/ -r -d docs Readme.md'
+	]));
+
+
+	/*
+	gulp.task('browser-sync', function() {
+		browserSync.init({
+	        server: {
+	            baseDir: "./"
+	        }
+	    });
+	});
+	*/
+
+	gulp.task('default', sequence(
+		
+		['setup', 'init-build'],
+
+		['build-images', 'build-fonts', 'validate-scripts'],
+
+		'build-pages',
+
+		['validate-tests', 'docs']
+	));
+
+
+	gulp.task('automate', function() { //not updated with latest changes
+			
+			gulp.watch('src/**/*.html', ['pages']);
+			
+			gulp.watch('src/assets/img/*.*', ['images']);
+			
+			gulp.watch('src/assets/js/**/*.js', ['scripts']);
+			
+			gulp.watch('src/assets/css/**/*.css', ['styles']);
+			
+			gulp.watch('src/test/*_test.js', ['tests']);
+	});
+
+	/*
+	 //example using sourcemaps, maybe return to this later
+	 gulp.task('scripts', ['jshint'], function(){
+		return gulp.src(path.src.js)
+			.pipe(sourcemaps.init())
+				.pipe(concat('main.min.js'))
+				.pipe(uglify())
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest(path.build.js));
+	});
+	*/ 
